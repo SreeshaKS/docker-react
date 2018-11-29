81c8b857f16beab94b2ebe708a6e4897e03a8a4d
Sreesha S <sreesha.s@bookmyshow.com>
2018-11-29 18:40:58 +0530

    Intial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Bye There - I changed
           </a>
         </header>
       </div>
